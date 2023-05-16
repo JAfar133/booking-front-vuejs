0ecb49caf966b8cc6f01d5d0c04a65fc7e7607ea
Misha Golovach <golovach.miha01@gmail.com>
2023-05-16 22:34:14 +0300

    "refactor code"

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+remote_host="195.133.49.102"
+remote_user="root"
+ssh_port="22"
+
+ssh -i ssh_key "$remote_user"@"$remote_host" << EOF
+echo "Starting deployment..."
+
+cd booking-front-vuejs
+git pull
+
+echo "Starting build..."
+npm run build
+echo "App was built"
+
+sudo cp -r /root/booking-front-vuejs/dist/* /var/www/booking/
+EOF
+
+
---
+++ scripts/java-deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+echo "Starting deployment..."
+
+remote_host="195.133.49.102"
+remote_user="root"
+ssh_port="22"
+
+echo "connect to server..."
+
+ssh -i ssh_key "$remote_user"@"$remote_host" << EOF
+  cd booking-backend-java
+  git pull
+
+  chmod +x mvnw
+  ./mvnw clean package
+
+  java -jar target/bookingSite-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+  if [ $? -eq 0 ]; then
+      echo "Application is now running on port 8080"
+      ps -ef | grep java
+  fi
+EOF
