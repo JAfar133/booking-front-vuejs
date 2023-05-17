5de1692821c71faf1322d88f980d7235e1aeb7d3
Misha Golovach <golovach.miha01@gmail.com>
2023-05-17 22:42:16 +0300

    "adaptive design + vuetify"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,9 @@
 remote_host="195.133.49.102"
 remote_user="root"
 ssh_port="22"
+ssh_path="C:/Users/golov/.ssh/ssh_key"
 
-ssh -i ssh_key "$remote_user"@"$remote_host" << EOF
+ssh -i $ssh_path "$remote_user"@"$remote_host" << EOF
 echo "Starting deployment..."
 cd booking-front-vuejs
 git pull
